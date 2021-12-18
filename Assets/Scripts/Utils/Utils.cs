add2b4a4dfec591a561e19b713ff4f2dfecbe26f
IndieGabo <indiegabodev@gmail.com>
2021-12-18 12:14:23 -0300

    Docummenting Utils

--- Assets/Scripts/Utils/Utils.cs
+++ Assets/Scripts/Utils/Utils.cs
@@ -26,6 +26,13 @@ public class Utils
         return converted;
     }
 
+    /// <summary> This function converts a given value's scale into a given another</summary>
+    /// <param name="value">The value wich should be rescaled</param>
+    /// <param name="maxValue">The maximum value it would have on it's current scale</param>
+    /// <param name="min">New scale minimum value</param>
+    /// <param name="max">New scale maximum value</param>
+    /// <param name="maxRounding"> A value to round up to max when met </param>
+    /// <returns>A float number representing the value rescaled</returns>
     public static float convertScale(float value, float maxValue, float min, float max, float maxRounding)
     {
         float delta = max - min;
