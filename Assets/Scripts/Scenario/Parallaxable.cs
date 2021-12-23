7329cd4f5fc5cff95fba5e6608115f5eed4d23ed
IndieGabo <indiegabodev@gmail.com>
2021-12-23 01:25:24 -0300

    testing parallax

--- Assets/Scripts/Scenario/Parallaxable.cs
+++ Assets/Scripts/Scenario/Parallaxable.cs
@@ -1,24 +1,34 @@
 using UnityEngine;
 
-public class Parallaxable
+public class Parallaxable : MonoBehaviour
 {
-    private Transform _layerTransform;
-    private float _scale;
+    [Header("Needed Objects")]
+    [SerializeField] private Transform _subject;
+    private float _smoothing = 0.3f;
+    private Camera _camera;
+    private Vector2 _startPos;
+    private float _startingZ;
+    private float _travel => this._camera.transform.position.x - this._startPos.x;
+    private float _distanceFromSubject => this.transform.position.z - this._subject.transform.position.z;
+    private float _clippingPlane => this._camera.transform.position.z + this._camera.farClipPlane;
+    // private float _clippingPlane => (this._camera.transform.position.z + (this._distanceFromSubject > 0 ? this._camera.farClipPlane : this._camera.nearClipPlane));
+    private float _parallaxFactor => Mathf.Abs(this._distanceFromSubject) / this._clippingPlane;
 
-    public Parallaxable(Transform layerTransform, float scale)
+    private void Awake()
     {
-        this._layerTransform = layerTransform;
-        this._scale = scale;
+        this._camera = Camera.main;
     }
 
-    public Transform layerTransform
+    private void Start()
     {
-        get { return this._layerTransform; }
-        set { this._layerTransform = value; }
+        this._startPos = this.transform.position;
+        this._startingZ = this.transform.position.z;
     }
-    public float scale
+
+    private void Update()
     {
-        get { return this._scale; }
-        set { this._scale = value; }
+        float newX = this._startPos.x + this._travel * this._parallaxFactor * this._smoothing;
+        this.transform.position = new Vector3(newX, this._startPos.y, this._startingZ);
     }
+
 }
