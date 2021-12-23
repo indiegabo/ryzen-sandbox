63ae12ce7fd00ce31726b7f4f752c25f9166f83b
IndieGabo <indiegabodev@gmail.com>
2021-12-23 17:25:36 -0300

    Refactoring some stuff

--- Assets/Scripts/Scenario/Parallaxable.cs
+++ Assets/Scripts/Scenario/Parallaxable.cs
@@ -8,10 +8,10 @@ public class Parallaxable : MonoBehaviour
     private Camera _camera;
     private Vector2 _startPos;
     private float _startingZ;
-    private float _travel => this._camera.transform.position.x - this._startPos.x;
-    private float _distanceFromSubject => this.transform.position.z - this._subject.transform.position.z;
-    private float _clippingPlane => (this._camera.transform.position.z + (this._distanceFromSubject > 0 ? this._camera.farClipPlane : this._camera.nearClipPlane));
-    private float _parallaxFactor => Mathf.Abs(this._distanceFromSubject / this._clippingPlane);
+    private float travel => this._camera.transform.position.x - this._startPos.x;
+    private float distanceFromSubject => this.transform.position.z - this._subject.transform.position.z;
+    private float clippingPlane => (this._camera.transform.position.z + (this.distanceFromSubject > 0 ? this._camera.farClipPlane : this._camera.nearClipPlane));
+    private float parallaxFactor => Mathf.Abs(this.distanceFromSubject) / this.clippingPlane;
 
     private void Awake()
     {
@@ -24,9 +24,9 @@ public class Parallaxable : MonoBehaviour
         this._startingZ = this.transform.position.z;
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
-        float newX = this._startPos.x + this._travel * this._parallaxFactor * this._smoothing;
+        float newX = this._startPos.x + this.travel * this.parallaxFactor * this._smoothing;
         this.transform.position = new Vector3(newX, this._startPos.y, this._startingZ);
     }
 
